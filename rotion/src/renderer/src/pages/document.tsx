f724eb33c431e4a131551e737a9aa29302f99589
marcoscode404 <viniciusmg99@outlook.com>
2023-01-27 13:17:49 -0400

    gap entre elementos

--- rotion/src/renderer/src/pages/document.tsx
+++ rotion/src/renderer/src/pages/document.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export function Document() {
   return (
-    <main className="flex-1 flex py-12 px-10">
+    <main className="flex-1 flex py-12 px-10 gap-8">
       Document
       <Link to="/">Acessar blank</Link>
     </main>
